870da251481fc0c3d3c1d93a6e6f00247b2791bb
unknown <mdixit@TavWITPD041.internal.tavisca.com>
2015-07-07 09:38:55 +0530

    Pushing operator Overloading

---
+++ Solution/OperatorOverloading/ClassReactangle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class ClassRectangle
+    {
+
+        public double Breadth { get; set; }
+        public double Length { get; set; }
+    
+        private double Area;
+        public void setArea() {
+            try { this.Area = this.Length * this.Breadth;
+            if (double.IsInfinity(Area))
+            {
+                throw new System.OverflowException("OverFlow");
+            }
+            }
+            catch (OverflowException E)
+            { Console.WriteLine("The Value Entered is Incorrect " );
+            Console.Error.WriteLine(E.StackTrace);
+            }
+        }
+
+        public double getArea() { return Area; }
+  
+
+
+    public static ClassRectangle operator +(ClassRectangle rectangleOne, ClassRectangle rectangleTwo)
+    {
+        ClassRectangle NewReactangle = new ClassRectangle();
+        try
+        {
+            NewReactangle.Length = rectangleOne.Length + rectangleTwo.Length;
+            NewReactangle.Breadth = rectangleOne.Breadth + rectangleTwo.Breadth;
+            if (double.IsInfinity(NewReactangle.Length) || double.IsInfinity(NewReactangle.Breadth))
+            {
+                throw new System.OverflowException("OverFlow");
+            }
+        }
+        catch (OverflowException E)
+        {
+            Console.WriteLine("The Value Entered is Incorrect ");
+            Console.Error.WriteLine(E.StackTrace);
+       
+        }
+       
+        return NewReactangle;
+    }
+       
+
+      
+
+        
+
+    }
+}
---
+++ Solution/OperatorOverloading/MainClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+
+namespace ConsoleApplication3
+{
+    class MainClass
+    {
+
+        static void Main(string[] args)
+        {
+            ClassRectangle RectangleOne = new ClassRectangle();
+            ClassRectangle RectangleTwo = new ClassRectangle();
+            ClassRectangle RectangleThree = new ClassRectangle();
+            
+            RectangleOne.Breadth = 6;
+            RectangleOne.Length = 6;
+            RectangleTwo.Breadth = double.MaxValue;
+            RectangleTwo.Length = double.MaxValue;
+
+
+
+            RectangleOne.setArea();
+            RectangleTwo.setArea();
+            Console.WriteLine("The Length and Breadth of Rectangle 1 is:{0}   ,,  {1}  and the Area is :{2}",RectangleOne.Length,RectangleOne.Breadth,RectangleOne.getArea());
+         
+            Console.WriteLine("The Length and Breadth of Rectangle 2 is {0}  ,,   {1}  and the Area is {2}", RectangleTwo.Length, RectangleTwo.Breadth, RectangleOne.getArea());
+         
+            RectangleThree = RectangleOne + RectangleTwo;
+            RectangleThree.setArea();
+            Console.WriteLine("The Length and Breadth of Rectangle 2 is {0}  ,,   {1}  and the Area is {2}", RectangleThree.Length, RectangleThree.Breadth, RectangleThree.getArea());
+       
+            
+            Console.ReadKey();
+        }
+    }
+}
